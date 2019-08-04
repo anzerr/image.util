3bc46f3a5aaf6296c7f00d4e5033c11d1b3f221d
ryan anzer <ryan.anzer@epitech.eu>
2019-08-04 02:56:25 +0200

    🚧 update text draw

--- src/text.js
+++ src/text.js
@@ -22,39 +22,42 @@ class Text {
 		return (this.char = p);
 	}
 
-	drawChar(char, x, y, size, color) {
+	drawChar(c, x, y, size, color) {
 		if (!this.char) {
 			throw new Error('missing char map try loading before draw');
 		}
-		for (let i = 0; i < this.char[char].length; i++) {
-			for (let v = 0; v < this.char[char][i].length; v++) {
-				if (this.char[char][i][v]) {
+		let char = this.char[c], map = char.bitmap, offset = char.offset;
+		for (let i = 0; i < map.length; i++) {
+			for (let v = 0; v < map[i].length; v++) {
+				if (map[i][v]) {
 					if (size === 1) {
-						this.map.set(x + (i * size), y + (v * size), color);
+						this.map.set(x + (i * size) + offset.x, y + (v * size) + offset.y, color);
 					} else {
-						this.map.fill(x + (i * size), y + (v * size), color, size);
+						this.map.fill(x + (i * size) + offset.x, y + (v * size) + offset.y, color, size);
 					}
 				}
 			}
 		}
+		return char.width;
 	}
 
 	draw(text, x, y, size = 1, color = WHITE) {
 		if (!this.char) {
 			throw new Error('missing char map try loading before draw');
 		}
-		let lines = text.split('\n'), s = 10 * size;
+		let lines = text.split('\n'), s = 10 * size, max = 0;
 		for (let v = 0; v < lines.length; v++) {
 			let buff = Buffer.from(lines[v]), offset = 0;
 			for (let i = 0; i < buff.length; i++) {
 				if (buff[i] === 9) {
 					offset += (s * 4);
 				} else {
-					this.drawChar(buff[i], x + (v * s), y + offset, size, color);
-					offset += s;
+					offset += this.drawChar(buff[i], x + (v * s), y + offset, size, color) * size;
 				}
 			}
+			max = Math.max(max, offset);
 		}
+		return [lines.length * s, max];
 	}
 
 }
